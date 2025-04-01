c7751a639e11d5af84023e3c04dc30653012c710
BrunoPascoa76 <brunopascoa03@ua.pt>
2025-04-01 16:50:31 +0100

    did a skeleton for the statistics

--- bus/bus_sim.py
+++ bus/bus_sim.py
@@ -23,37 +23,39 @@ MAX_INSPECTION_TIME = 1.05*60
 REPAIR_PROBABILITY = 0.30
 MIN_REPAIR_TIME = 2.1*60
 MAX_REPAIR_TIME = 4.5*60
-RANDOM_SEED = 107418
 SIM_TIME=160*60
 
 def bus(env, id, inspection_stations, repair_stations):
     arrival_time=env.now
 
+
     with inspection_stations.request() as req:
         yield req # wait in the inspection queue (if needed)
         exit_queue_time=env.now
-        debug(f"{env.now}: Bus {id} starts inspection after waiting for {exit_queue_time-arrival_time:.2f} minutes")
+        info(f"{env.now}: Bus {id} starts inspection after waiting for {exit_queue_time-arrival_time:.2f} minutes")
 
         yield env.timeout(rng.uniform(MIN_INSPECTION_TIME,MAX_INSPECTION_TIME)) # simulate inspection time
         leave_inspection_time=env.now
-        debug(f"{env.now}: Bus {id} finishes inspection (took {leave_inspection_time-exit_queue_time:.2f} minutes)")
+        info(f"{env.now}: Bus {id} finishes inspection (took {leave_inspection_time-exit_queue_time:.2f} minutes)")
 
     if rng.random() <= REPAIR_PROBABILITY: # decide whether or not it needs repair
-        debug(f"{env.now}: Bus {id} needs repair")
-
+        info(f"{env.now}: Bus {id} needs repair")
+        
         with repair_stations.request() as req:
             repair_arrival_time=env.now
 
+
             yield req
+
             exit_queue_time=env.now
-            debug(f"{env.now}: Bus {id} starts repair after waiting for {exit_queue_time-repair_arrival_time:.2f} minutes")
+            info(f"{env.now}: Bus {id} starts repair after waiting for {exit_queue_time-repair_arrival_time:.2f} minutes")
             
             yield env.timeout(rng.uniform(MIN_REPAIR_TIME,MAX_REPAIR_TIME)) # simulate repair time
             leave_repair_time=env.now
-            debug(f"{env.now}: Bus {id} finishes repair and leaves (took {leave_repair_time-exit_queue_time:.2f} minutes)")
+            info(f"{env.now}: Bus {id} finishes repair and leaves (took {leave_repair_time-exit_queue_time:.2f} minutes)")
 
     else:
-        debug(f"{env.now}: Bus {id} does not need repair and leaves")
+        info(f"{env.now}: Bus {id} does not need repair and leaves")
 
 def bus_generator(env,inspection_stations,repair_stations):
     """Generates new buses at exponentially distributed intervals"""
@@ -62,15 +64,14 @@ def bus_generator(env,inspection_stations,repair_stations):
         pre_wait_time=env.now
         yield env.timeout(rng.expovariate(1/MEAN_INTERARRIVAL_TIME)) # wait for next bus
 
-        debug(f"{env.now}: Waited {env.now-pre_wait_time:.2f} minutes for next bus")
+        info(f"{env.now}: Waited {env.now-pre_wait_time:.2f} minutes for next bus")
 
         bus_id+=1
-        debug(f"{env.now}: Bus {bus_id} arrives for inspection")
+        info(f"{env.now}: Bus {bus_id} arrives for inspection")
         env.process(bus(env,bus_id,inspection_stations,repair_stations))
 
 
 def simulate():
-    rng.seed(RANDOM_SEED)
     env=simpy.Environment()
 
     parameters={
@@ -82,7 +83,6 @@ def simulate():
         "repair_probability": REPAIR_PROBABILITY,
         "min_repair_time": MIN_REPAIR_TIME,
         "max_repair_time": MAX_REPAIR_TIME,
-        "random_seed": RANDOM_SEED,
         "sim_time": SIM_TIME
     }
 
@@ -91,7 +91,8 @@ def simulate():
 
     env.process(bus_generator(env,inspection_stations,repair_stations))
 
-    debug(f"{0}: Starting simulation with parameters {parameters}")
+
+    info(f"{0}: Starting simulation with parameters {parameters}")
     env.run(until=SIM_TIME)
 
 
@@ -120,8 +121,7 @@ if __name__=="__main__":
     MAX_INSPECTION_TIME=args.max_inspection_time
     REPAIR_PROBABILITY=args.repair_probability
     MIN_REPAIR_TIME=args.min_repair_time
-    MAX_REPAIR_TIME=args.max_repair_time
-    
+    MAX_REPAIR_TIME=args.max_repair_time    
 
     if(MIN_INSPECTION_TIME > MAX_INSPECTION_TIME):
         parser.error("--min_inspection_time must be less than --max_inspection_time")
